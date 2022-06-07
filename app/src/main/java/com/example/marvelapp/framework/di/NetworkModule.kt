07523f9c8e22f368b311d3c1a35f81c78257d212
João Paulo <joaopaulokimi@gmail.com>
2022-06-06 22:35:34 -0300

    Criando módulo di para a camada de network

---
+++ app/src/main/java/com/example/marvelapp/framework/di/NetworkModule.kt
@@ -0,0 +1,56 @@
+package com.example.marvelapp.framework.di
+
+import com.example.marvelapp.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(Singleton::class)
+object NetworkModule {
+
+    @Provides
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            setLevel(
+                if (BuildConfig.DEBUG) {
+                    HttpLoggingInterceptor.Level.BODY
+                } else HttpLoggingInterceptor.Level.NONE
+            )
+        }
+    }
+
+    @Provides
+    fun provideOkHttpClient(
+        loggingInterceptor: HttpLoggingInterceptor,
+    ): OkHttpClient {
+        return OkHttpClient().newBuilder()
+            .addInterceptor(loggingInterceptor)
+            .readTimeout(15,TimeUnit.SECONDS)
+            .connectTimeout(15,TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    fun provideGsonConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+    @Provides
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        converterFactory: GsonConverterFactory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://gateway.marvel.com:443/v1/public/")
+            .client(okHttpClient)
+            .addConverterFactory(converterFactory).build()
+    }
+}
---
+++ app/src/main/java/com/example/marvelapp/presentation/characters/CharactersViewHolder.kt
@@ -0,0 +1,9 @@
+package com.example.marvelapp.presentation.characters
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.marvelapp.databinding.FragmentCharactersBinding
+
+class CharactersViewHolder(
+    itemCharactersBinding: ItemCharacterBiding
+): RecyclerView.ViewHolder() {
+}
